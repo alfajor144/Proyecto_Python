f3c9bba3eb794c36b8974dbd71906b97a08fa592
alfajor <alfa@gmail.com>
2020-09-24 22:41:57 -0300

    subidas las paginas

--- biblioteca/apps/libro/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Autor
-
-class AutorForms(forms.ModelForm):
-    class Meta:
-        model = Autor
-        fields = ['nombre', 'apellido', 'nacionalidad', 'descripcion']
