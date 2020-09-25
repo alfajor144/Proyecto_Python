c02df22433b1704076904f1259fa7f543dd0d850
Julio Arrieta <julioarrieta23@gmail.com>
2020-09-25 19:07:58 -0300

    cambiando urls.py

--- UruguayJob/apps/Usuario/urls.py
+++ UruguayJob/apps/Usuario/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeInvitado, name='Invitado'), #Home
-    path('u/', views.HomeUser, name='Usuario'), 
-    path('a/', views.HomeAdmin, name='Administrador'),
-    path('r/', views.Registrarse, name='Registrarse'),
-    path('e/', views.Estadistica, name='Estadistica'),
-    path('n/', views.Entrevistar, name='Entrevistar'),
-    path('s/', views.Entrevistas, name='Entrevistas'),
-    path('c/', views.Calificar, name='Calificar'),
-    path('i/', views.IniciarSesion, name='IniciarSesion')
+    path('usuario/', views.HomeUser, name='Usuario'), 
+    path('administrador/', views.HomeAdmin, name='Administrador'),
+    path('registrarse/', views.Registrarse, name='Registrarse'),
+    path('estadistic/', views.Estadistica, name='Estadistica'),
+    path('entrevistar/', views.Entrevistar, name='Entrevistar'),
+    path('enrevistas/', views.Entrevistas, name='Entrevistas'),
+    path('calificar/', views.Calificar, name='Calificar'),
+    path('iniciar-sesion/', views.IniciarSesion, name='IniciarSesion')
 ]
