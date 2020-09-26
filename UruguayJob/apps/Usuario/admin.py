21947be4dff6076ab569fa6da922b36d4a7e6d7b
alfajor <alfa@gmail.com>
2020-09-25 22:03:27 -0300

    listo los modelos

--- UruguayJob/apps/Usuario/admin.py
+++ UruguayJob/apps/Usuario/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Usuario, Oferta, Postulacion
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Oferta)
+admin.site.register(Postulacion)
