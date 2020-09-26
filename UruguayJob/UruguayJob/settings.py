c6947663119f42beec8bd10289897202e66fc032
alfajor <alfa@gmail.com>
2020-09-25 22:07:23 -0300

    fix model, deleted app ofertas

--- UruguayJob/UruguayJob/settings.py
+++ UruguayJob/UruguayJob/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.Oferta',
     'apps.Usuario',
     'django.contrib.admin',
     'django.contrib.auth',
--- UruguayJob/apps/Usuario/models.py
+++ UruguayJob/apps/Usuario/models.py
@@ -5,7 +5,7 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length = 200, blank = False, null = False)
     apellido = models.CharField(max_length = 200, blank = False, null = False)
     email = models.EmailField(max_length = 254)
-
+    cv = models.CharField(max_length = 300, blank = True, null = True)
     def __str__(self):
         return self.nombre
 
